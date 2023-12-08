df73570d29106a8f7709fcf6068cff8206813d1c
Antoni Vidal <anvifer2@upv.es>
2023-12-08 16:29:24 +0100

    Small changes in some input files.

--- run_Seabrog.sh
+++ run_Seabrog.sh
@@ -3,4 +3,8 @@
 #$ -l h_rt=48:00:00  # Tiempo requerido (hh:mm:ss).
 
 echo '3D_Seaborg'
-./femffusion.exe -f 3D_Seaborg/seaborg_SP1_fe2.prm -type_prec bad-broyden -init_prec gs-cgilu
+./femffusion.exe -f 3D_Seaborg/periferal_fast/periferal_seaborg_SP1_p10.prm
+./femffusion.exe -f 3D_Seaborg/periferal_fast/periferal_seaborg_SP1_p5.prm
+./femffusion.exe -f 3D_Seaborg/seaborg_SP1_fe1_p20.prm
+./femffusion.exe -f 3D_Seaborg/seaborg_SP1_fe1_p10.prm
+./femffusion.exe -f 3D_Seaborg/seaborg_SP1_fe1_p40.prm
